9041fa3723097b6294307f525d1b69c317d00a5a
Parth Parmar <parth3rdeye@gmail.com>
2020-05-21 17:31:50 +0530

    Updates

---
+++ precache-manifest.70c458b0310f3afb7cdbeee78d8419c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ef28a5263d553bd013d4b8d171b131f",
+    "url": "/react-simple-ecommerce/index.html"
+  },
+  {
+    "revision": "f0110131927016327386",
+    "url": "/react-simple-ecommerce/static/css/main.13b4709e.chunk.css"
+  },
+  {
+    "revision": "81e53196672486691770",
+    "url": "/react-simple-ecommerce/static/js/2.60e647af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-simple-ecommerce/static/js/2.60e647af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0110131927016327386",
+    "url": "/react-simple-ecommerce/static/js/main.fddbf085.chunk.js"
+  },
+  {
+    "revision": "0972b20fc58c7be66e30",
+    "url": "/react-simple-ecommerce/static/js/runtime-main.a9df08c1.js"
+  }
+]);
