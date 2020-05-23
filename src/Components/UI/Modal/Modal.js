1b9f6af52e32e0f40475e6b566f4fd4c678704d7
Parth Parmar <parth3rdeye@gmail.com>
2020-05-23 15:34:03 +0530

    added header menu

--- src/Components/UI/Modal/Modal.js
+++ src/Components/UI/Modal/Modal.js
@@ -1,21 +1,25 @@
 import React from "react";
 import classes from "./Modal.module.css";
+import Aux from "../../../HOC/Auxilary";
+import Backdrop from "../Backdrop/Backdrop";
 
 const modal = (props) => {
   return (
-    <div
-      className={classes.Modal}
-      style={{
-        //display: props.isShow ? "block" : "none",
-        transform: props.isShow ? "translateY(0)" : "translateY(-100vh)",
-        opacity: props.isShow ? "1" : "0",
-      }}
-    >
-      <button className={classes.closeButton} onClick={props.clicked}>
-        X
-      </button>
-      {props.children}
-    </div>
+    <Aux>
+      <Backdrop show={props.isShow} clicked={props.clicked} />
+      <div
+        className={classes.Modal}
+        style={{
+          transform: props.isShow ? "translateY(0)" : "translateY(-100vh)",
+          opacity: props.isShow ? "1" : "0",
+        }}
+      >
+        <button className={classes.closeButton} onClick={props.clicked}>
+          X
+        </button>
+        {props.children}
+      </div>
+    </Aux>
   );
 };
 
