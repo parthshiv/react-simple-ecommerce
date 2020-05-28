c3e27de9cd2cbf0ac61d55fb10dcdbea6b8e7904
Parth Parmar <parth3rdeye@gmail.com>
2020-05-28 17:40:29 +0530

    Updates

---
+++ precache-manifest.79d5088c24551993ebb51c477f648950.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b739c9198b1e88d268598795ea2b01a",
+    "url": "/react-simple-ecommerce/index.html"
+  },
+  {
+    "revision": "103628a100b8b954b3ea",
+    "url": "/react-simple-ecommerce/static/css/main.5f89c5a9.chunk.css"
+  },
+  {
+    "revision": "a6ae24b565db31650727",
+    "url": "/react-simple-ecommerce/static/js/2.0afbe448.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-simple-ecommerce/static/js/2.0afbe448.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "103628a100b8b954b3ea",
+    "url": "/react-simple-ecommerce/static/js/main.2a237e63.chunk.js"
+  },
+  {
+    "revision": "0972b20fc58c7be66e30",
+    "url": "/react-simple-ecommerce/static/js/runtime-main.a9df08c1.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/react-simple-ecommerce/static/media/burger-logo.b8503d26.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-simple-ecommerce/precache-manifest.dc0ef866d646a1d930e409e09e2fdade.js"
+  "/react-simple-ecommerce/precache-manifest.79d5088c24551993ebb51c477f648950.js"
 );
 
 self.addEventListener('message', (event) => {
